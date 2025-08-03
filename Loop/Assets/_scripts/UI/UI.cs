824685780bb7edf627cfd3145dc657962855898e
kellenstrydom <2559739@students.wits.ac.za>
2025-08-03 18:45:41 +0200

    pusu

---
+++ Loop/Assets/_scripts/UI/UI.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UI : MonoBehaviour
+{
+    private void Awake()
+    {
+        //throw new NotImplementedException();
+    }
+
+
+    private void OnEnable()
+    {
+    }
+
+
+    void Pause(InputAction.CallbackContext context)
+    {
+        
+    }
+}
